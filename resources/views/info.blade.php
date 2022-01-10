0365dfb632277b7c6cf6df119ee0873f909dce1c
Edward Tanguay (hm.win) <edward@tanguay.info>
2022-01-10 13:17:01 +0100

    initial commit

---
+++ resources/views/info.blade.php
@@ -0,0 +1,19 @@
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Laravel Site</title>
+</head>
+<body>
+	<h1>Info</h1>
+	<p>This is the info page.</p>
+	<p>Go back to the <a href="/">Welcome Page</a>.</p>
+	<ul>
+		<li>Go to <a href="articles/123">Article #123</a></li>
+		<li>Go to <a href="articles/234">Article #234 in English</a></li>
+		<li>Go to <a href="articles/234/german">Article #234 in German</a></li>
+		<li>Go to <a href="toparticle">Top article</a> (redirect)</li>
+	</ul>
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/info', function () {
+    return view('info');
+});
+
+Route::get('/articles/{id}/{lang?}', function ($id, $lang = 'english') {
+    return ("showing article #$id ($lang)");
+})->where('id', '[0-9]+')->where('lang', '(english|german)');
+
+Route::redirect('/toparticle', '/articles/555');
